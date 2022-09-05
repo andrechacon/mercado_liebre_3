662d439a1d027281d72558dc08f55ce03d60d0f8
andrechacon <achacon190@gmail.com>
2022-09-04 22:51:32 -0300

    subiendo

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require ("express");
 const app = express(); 
 const path= require('path'); 
-const PORT= 3000; 
+const PORT= 3300; 
 
 app.use(express.static(path.join(__dirname, 'public'))); 
 
